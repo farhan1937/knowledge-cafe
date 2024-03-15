254f92f88edfd71a6e4e06fb1f4b8b79de96180c
Farhan <farhansadik1937@gmail.com>
2024-03-16 02:03:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Blogs from './compunents/Blogs/Blogs'
+import Header from './compunents/header'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header></Header>
+    <Blogs></Blogs>
+     
+      
+      
+    </>
+  )
+}
+
+export default App
